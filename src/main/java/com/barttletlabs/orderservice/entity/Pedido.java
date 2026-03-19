09c2af829a9fe74b946e79de60cb0ef0fc6ffc81
Carlos Daniel Mejia Moreno <charliebarttlet@gmail.com>
2026-03-19 11:27:58 -0600

    feat(h2_broker): set up.

---
+++ src/main/java/com/barttletlabs/orderservice/entity/Pedido.java
@@ -0,0 +1,59 @@
+package com.barttletlabs.orderservice.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombreCliente;
+    private String pizza;
+    private Double precio;
+    private String estado;
+
+    public Pedido() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(String pizza) {
+        this.pizza = pizza;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
